17ed2ab1c7f64256a97afee38e0cc2a3cac390fb
Satyakam Mohapatra <mohapatra.satyakam91@gmail.com>
2018-05-13 04:29:14 +0530

    initial Commit

---
+++ Spring5WebProject/src/main/java/com/satyakm/Spring5WebProject/Spring5WebProjectApplication.java
@@ -0,0 +1,12 @@
+package com.satyakm.Spring5WebProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebProjectApplication.class, args);
+	}
+}
